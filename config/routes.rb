0bd548304dbd314a58df43b170e31b7d89eae84d
hham3 <hhamilto@citadel.edu>
2020-04-20 19:34:08 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get '/login', to: 'login#home'
+  get '/logout', to: 'login#logout'
+  get '/classSchedule', to: 'user#summary', as: 'summary'
+  get '/edit/:classSchedule', to: 'classSchedule#edit', as: 'add_course'
+  get '/delete', to: 'classSchedule#edit', as: 'delete_course'
+  
 end
