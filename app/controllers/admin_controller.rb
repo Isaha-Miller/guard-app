90244bb9224fae8b08cc9c743cbb27360b410fac
Isaha Miller <ubuntu@ip-172-31-43-248.us-east-2.compute.internal>
2020-04-27 02:55:41 +0000

    added a filter function to the admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,29 +12,28 @@ class AdminController < ApplicationController
             flash[:notice] = "Invalid form"
             flash[:color] = "invalid"
         end
-    
     render "new"
     
     end
-    def search "adds a case insensitive search funtion"
-        @students = Student.all
+    
+    def search #adds a case insensitive search funtion
+        @students = User.all
         if !(params[:first_name].to_s.empty?)
-            @students = Student.where('lower(first_name) = ?', params[:first_name].downcase)
+            @students = User.where('lower(first_name) = ?', params[:first_name].downcase)
             #filter_options[:first_name] = params[:first_name].downcase
         end
         if !(params[:last_name].to_s.empty?)
-            @students = Student.where('lower(last_name) = ?', params[:last_name].downcase)
+            @students = User.where('lower(last_name) = ?', params[:last_name].downcase)
         end
         if !(params[:email].to_s.empty?)
-            @students = Student.where('lower(email) = ?', params[:email].downcase)
+            @students = User.where('lower(email) = ?', params[:email].downcase)
         end
         if !(params[:cwid].to_s.empty?)
-            @students = Student.where('lower(cwid) = ?', params[:cwid].downcase)
+            @students = User.where('lower(cwid) = ?', params[:cwid].downcase)
         end
         if !(params[:phone_number].to_s.empty?)
-            @students = Student.where('lower(phone_number) = ?', params[:phone_number].downcase)
+            @students = Usert.where('lower(phone_number) = ?', params[:phone_number].downcase)
         end
         
     end
-        
 end
