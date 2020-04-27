5d32f10ea5fa0038aa3ef6bc0d21a17a055fed71
Isaha Miller <ubuntu@ip-172-31-43-248.us-east-2.compute.internal>
2020-04-27 01:23:52 +0000

    -a

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -16,4 +16,25 @@ class AdminController < ApplicationController
     render "new"
     
     end
+    def search "adds a case insensitive search funtion"
+        @students = Student.all
+        if !(params[:first_name].to_s.empty?)
+            @students = Student.where('lower(first_name) = ?', params[:first_name].downcase)
+            #filter_options[:first_name] = params[:first_name].downcase
+        end
+        if !(params[:last_name].to_s.empty?)
+            @students = Student.where('lower(last_name) = ?', params[:last_name].downcase)
+        end
+        if !(params[:email].to_s.empty?)
+            @students = Student.where('lower(email) = ?', params[:email].downcase)
+        end
+        if !(params[:cwid].to_s.empty?)
+            @students = Student.where('lower(cwid) = ?', params[:cwid].downcase)
+        end
+        if !(params[:phone_number].to_s.empty?)
+            @students = Student.where('lower(phone_number) = ?', params[:phone_number].downcase)
+        end
+        
+    end
+        
 end
